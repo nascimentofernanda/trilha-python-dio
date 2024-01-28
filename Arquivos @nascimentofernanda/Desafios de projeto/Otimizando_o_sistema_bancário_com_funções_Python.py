5efd8fbac6d595eefd365b3bde67e33821ee88e1
fercfn <nascimentofernandadev@gmail.com>
2024-01-28 13:33:53 -0300

    alterações no desafio de otimização do sistema bancário

--- Arquivos @nascimentofernanda/Desafios de projeto/Otimizando_o_sistema_bancário_com_funções_Python.py
+++ Arquivos @nascimentofernanda/Desafios de projeto/Otimizando_o_sistema_bancário_com_funções_Python.py
@@ -17,7 +17,7 @@
 
 #DICIONÁRIOS E LISTAS
 clientes={
-    12345678900:{"nome":"Cliente 1", "conta-corrente":"000001", "conta-poupança":"100000"},
+    12345678900:{"nome":"Cliente 1", "número da conta":"000001", "saldo":"0.00"},
 }
 
 #STRINGS
@@ -47,8 +47,7 @@ x_menu=0
 inicio= f"""
 Olá, seja bem-vindo(a) ao nosso banco, para iniciar seu atendimento, por favor, escolha uma opção:
 [0] Quero acessar minha conta
-[1] Sou cliente mas quero abrir uma nova conta
-[2] Não sou cliente e quero abrir uma conta
+[1] Quero abrir uma conta
 """
 
 boas_vindas= f"""
@@ -65,7 +64,7 @@ def menu_principal(): #executa o menu principal com as opções de sacar, deposi
     while x_while!=0: #loop que executa o menu principal
         print(boas_vindas)
         opção_escolhida=int(input())
-        if opção_escolhida==0:
+        if opção_escolhida==0: # 0 é a opção para sair do atendimento
             print(AGRADECIMENTO)
             x_while=0
         elif opção_escolhida==1 or opção_escolhida==2 or opção_escolhida==3:
@@ -87,18 +86,6 @@ def buscar_cpf(): #executa a busca do cpf para identificar o cliente
         else:
             i=int(i)
             cliente=cliente+1
-
-def adicionar_conta(): #adiciona uma conta a um CPF que já tenha uma conta cadastrada
-    while cpf != usuários[i]:
-        i=i+1
-    else:
-        nova_conta = contas[-1]
-        nova_conta = nova_conta+1
-        contas.append(nova_conta)
-        nova_conta=str(nova_conta)
-        usuários[cpf][nova_conta]="0.00"
-        print(usuários["nome"], ", sua conta criada com sucesso!")
-        menu_principal()
 
 def criar_conta(): #cria uma nova conta (nova chave para o dicionário usuários)
     print("Por favor, digite seu cpf")
@@ -274,7 +261,7 @@ def extract(): #função que executa a opção de exibir o extrato das transaç�
 
 print(inicio)
 opção_inicial=int(input())
-while opção_inicial ==0 or opção_inicial ==1 or opção_inicial==2:
+while opção_inicial ==0 or opção_inicial ==1:
     if opção_inicial==0:
         print("Por favor, digite o número do seu CPF")
         cpf=int(input())
@@ -282,8 +269,6 @@ while opção_inicial ==0 or opção_inicial ==1 or opção_inicial==2:
         #se existir executa o menu principal, senão pede para o cliente redigitar o cpf
         menu_principal()
     elif opção_inicial==1:
-        adicionar_conta()
-    elif opção_inicial==2:
         criar_conta()
 else:
     print("Opção não disponível, por favor, escolha uma das opções.")
